d050f8114eed55c0bbdea3f46f39cb7c3e5cd0f3
UphillD <13258958+UphillD@users.noreply.github.com>
2020-04-05 21:24:32 +0300

    Add TLB plotters for the second question (IPC Reduction per memory doubling)

---
+++ TLB/plot_TLB_IPC_10pt.py
@@ -0,0 +1,92 @@
+# Advanced Computer Architecture, 2019-2020, 3.4.37.8
+# 1st Assignment
+
+# Plotting Code (TLB)
+# You shouldn't run this directly, run plotter_TLB.sh instead.
+# Dependencies: python, python-matplotlib
+
+import sys
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+x_Axis = []
+ipc_Axis = []
+mpki_Axis = []
+
+index = 0
+tlb_entries_base = 8
+tlb_assoc_base = 4
+tlb_psize_base = 4096
+
+for outFile in sys.argv[1:]:
+    fp = open(outFile)
+    line = fp.readline()
+    while line:
+        tokens = line.split()
+        if (line.startswith("Total Instructions: ")):
+            total_instructions = int(tokens[2])
+        elif (line.startswith("IPC:")):
+            ipc = float(tokens[1])
+        elif (line.startswith("  Data Tlb")):
+            sizeLine = fp.readline()
+            tlb_entries = int(sizeLine.split()[1])
+            bsizeLine = fp.readline()
+            tlb_psize = int(bsizeLine.split()[2])
+            assocLine = fp.readline()
+            tlb_assoc = int(assocLine.split()[1])
+        elif (line.startswith("Tlb-Total-Misses")):
+            tlb_total_misses = int(tokens[1])
+            tlb_miss_rate = float(tokens[2].split('%')[0])
+            mpki = tlb_total_misses / (total_instructions / 1000.0)
+
+        line = fp.readline()
+
+    fp.close()
+
+    index += (tlb_entries / tlb_entries_base) - 1
+    index += (tlb_assoc / tlb_assoc_base) - 1
+    index += (tlb_psize / tlb_psize_base) - 1
+
+    while index > 0:
+        ipc *= 90/100
+        index -= 1
+
+    tlbConfigStr = '{}.{}.{}B'.format(tlb_entries,tlb_assoc,tlb_psize)
+    print(tlbConfigStr)
+    x_Axis.append(tlbConfigStr)
+    ipc_Axis.append(ipc)
+    mpki_Axis.append(mpki)
+
+print(x_Axis)
+print(ipc_Axis)
+print(mpki_Axis)
+
+fig, ax1 = plt.subplots()
+ax1.grid(True)
+ax1.set_xlabel("TLB Size.Associativity.Page Size")
+
+xAx = np.arange(len(x_Axis))
+ax1.xaxis.set_ticks(np.arange(0, len(x_Axis), 1))
+ax1.set_xticklabels(x_Axis, rotation=45)
+ax1.set_xlim(-0.5, len(x_Axis) - 0.5)
+ax1.set_ylim(min(ipc_Axis) - 0.05 * min(ipc_Axis), max(ipc_Axis) + 0.05 * max(ipc_Axis))
+ax1.set_ylabel("$IPC$")
+line1 = ax1.plot(ipc_Axis, label="IPC", color="red",marker='x')
+
+ax2 = ax1.twinx()
+ax2.xaxis.set_ticks(np.arange(0, len(x_Axis), 1))
+ax2.set_xticklabels(x_Axis, rotation=45)
+ax2.set_xlim(-0.5, len(x_Axis) - 0.5)
+ax2.set_ylim(min(mpki_Axis) - 0.05 * min(mpki_Axis), max(mpki_Axis) + 0.05 * max(mpki_Axis))
+ax2.set_ylabel("$MPKI$")
+line2 = ax2.plot(mpki_Axis, label="TLB_MPKI", color="green",marker='o')
+
+lns = line1 + line2
+labs = [l.get_label() for l in lns]
+
+plt.title("IPC vs MPKI, 10% IPC Reduction")
+lgd = plt.legend(lns, labs)
+lgd.draw_frame(False)
+plt.savefig("TLB.png",bbox_inches="tight")
---
+++ TLB/plotter_TLB_IPC_10pt.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Advanced Computer Architecture, 2019-2020, 3.4.37.8
+# 1st Assignment
+
+# Plotter Script (TLB, 10% IPC reduction per memory doubling)
+# Install python-matplotlib before running.
+
+# This assumes a directory tree like the following:
+# Main Project dir  ${ACA_PATH}
+# ├ CSLab code dir  ${HLP_PATH}
+# ├ Parsec dir      ${PAR_PATH}
+# └ PIN dir         ${PIN_PATH}
+# It also assumes that you used the bencher_TLB.sh script for the benchmarks
+
+# ------------------------------------------------
+# You should only have to change things below here
+# ------------------------------------------------
+
+# Project directory
+ACA_PATH=~/Projects/advcomparch
+
+# Main directories
+PAR_PATH=${ACA_PATH}/parsec-3.0       # Parsec folder
+
+# ------------------------------------------------
+# You should only have to change things above here
+# ------------------------------------------------
+
+# Workspace paths
+WRK_PATH=${PAR_PATH}/parsec_workspace
+OUT_PATH=${WRK_PATH}/outputs/TLB
+
+# Benchmark array
+declare -a BenchArray=(
+        "blackscholes"
+#        "bodytrack"
+#        "canneal"
+#        "facesim"
+#        "ferret"
+#        "fluidanimate"
+#        "freqmine"
+#        "rtview"
+#        "streamcluster"
+#        "swaptions"
+)
+
+# Loop through the available benchmarks
+for bench in "${BenchArray[@]}"; do
+    python plot_TLB_IPC_10pt.py ${OUT_PATH}/${bench}_TLB*
+    mv TLB.png TLB_${bench}_IPC_10pt.png
+done
